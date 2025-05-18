beb3f20912ce0a1767e5ec67406550f8d8a0270f
Neel Mehendale <144788333+neelSchool@users.noreply.github.com>
2025-05-18 20:35:10 +0100

    done using floyd cycle detection

--- HappySadNumberTask/Program.cs
+++ HappySadNumberTask/Program.cs
@@ -2,29 +2,42 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static bool IsHappyNumber(int num)
+    {
+        //floyd cycle detection its more efficient space wise than hash set lookups
+        int slow = num, fast = num;
+        do
         {
-            /*There are said to be happy numbers and sad numbers.
-            Happy numbers will reduce to 1 when the digits belonging to the numbers are squared and added together to produce another number and the process is repeated. e.g. when we take 19
-            1 squared is 1 , 9 squared is 81. 81 +1 = 82
-            8 squared is 64, 2 squared is 4.  64 + 4 = 68
-            6 squared is 36, 8 squared is 64, 36 + 64 = 100 
-            1 squared is 1, 0 squared is 0 and thus the original number is reduced to 1
-            Sad numbers will not reduce to 1 and will loop endlessly. e.g. when we take 20
-            2 squared is 4, 0 squared is 0, 4 + 0 = 4
-            4 squared is 16, 0 squared is 0, 16 + 0 = 16
-            1 squared is 1, 6 squared is 36, 1 + 36 = 37
-            3 squared is 9, 7 squared is 49, 9 + 49 = 58
-            5 squared is 25, 8 squared is 64, 25 + 64 = 89
-            8 squared is 64, 9 squared is 81, 64 + 81 = 145
-            1 squared is 1, 4 squared is 16, 5 squared is 25, 1 + 16 + 25 = 42
-            4 squared is 16, 2 squared is 4, 16 + 4 = 20
-            20 will loop endlessly
-            */
-            //write a program to determine if a number is happy or sad
-            //The program should take a number as input and output whether the number is happy or sad
-            //test your program with the inputs of 19 and 20
-            //show your output in the readme file
+            slow = GetSumOfSquares(slow); // Moves one step
+            fast = GetSumOfSquares(GetSumOfSquares(fast)); // Moves two steps
         }
+        while (slow != fast); // Detects cycle
+        return slow == 1;
+    }
+    static int GetSumOfSquares(int num)
+    {
+        int sum = 0;
+        while (num > 0)
+        {
+            int digit = num % 10;
+            sum += digit * digit;
+            num /= 10;
+        }
+        return sum;
+    }
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int num = int.Parse(Console.ReadLine());
+        if (IsHappyNumber(num)){
+            Console.WriteLine($"{num} is a Happy Number!");
+        } else {
+            Console.WriteLine($"{num} is a Sad Number!");
+        }
+
+        Console.WriteLine("Testing with 19 and 20:");
+        Console.WriteLine(19 + " -> " + (IsHappyNumber(19) ? "Happy" : "Sad"));
+        Console.WriteLine(20 + " -> " + (IsHappyNumber(20) ? "Happy" : "Sad"));
+    }
     }
 }
